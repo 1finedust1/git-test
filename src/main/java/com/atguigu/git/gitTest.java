46a8b00c0090cfd71bdc53dbef294398c4fc0f0f
chasing <chasingwu@163.com>
2023-03-09 21:18:56 +0800

    third commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hi");
+        System.out.println("hello hello hi");
     }
 }
